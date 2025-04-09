bb57b05e709d9969ae6604721533b78ec7e8e32d
Johan <jsgalanh@ufpso.edu.co>
2025-04-08 20:40:14 -0500

    Inicio de sesion con js

--- js/loginService.js
+++ js/loginService.js
@@ -2,5 +2,11 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault()
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
-    alert('Estos son tus datos: email: ' + email + ' pass: ' + password)
+    if (email === '' || password === '') {
+        console.log('Por favor completar todos los campos.')
+    }else if (email === 'test@gmail.com' && password === '12345678' ) {
+        console.log('Inicio de sesion exitoso.')
+    }else{
+        console.log('Correo o contraseña incorrectos.')
+    } 
 })
